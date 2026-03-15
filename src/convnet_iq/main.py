c7be61134b3653da543d76af8de37bcbe13f95e8
rpw1134 <ryanw1134@gmail.com>
2026-03-15 19:03:46 -0400

    just more testing

--- src/convnet_iq/main.py
+++ src/convnet_iq/main.py
@@ -1,3 +1,6 @@
+from convnet_iq.iq_generation.maps import index_to_symbol
+
+
 def main():
     from .iq_generation.IQGenerator import IQGenerator
     gen = IQGenerator(seed=123)
@@ -5,7 +8,7 @@ def main():
     print("Generated QPSK IQ data (shape: {}):".format(iq_data.shape))
     print(iq_data)
     labels = gen.generate_softmax_indices_for_signals(iq_data, "QPSK")
-    print(labels)
+    print(index_to_symbol[labels])
 
 if __name__ == "__main__":
     main()
